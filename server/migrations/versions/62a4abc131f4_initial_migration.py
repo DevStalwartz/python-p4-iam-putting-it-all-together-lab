79bfb4ed07bf321f3b5e885fd93e077975280bc9
onesmus odongo <odongo1999@gmail.com>
2024-10-30 20:33:37 +0300

    update solution

--- server/migrations/versions/62a4abc131f4_initial_migration.py
+++ server/migrations/versions/62a4abc131f4_initial_migration.py
@@ -1,8 +1,8 @@
-"""Create users and recipes tables
+"""initial migration
 
-Revision ID: bcdfe35a4c42
+Revision ID: 62a4abc131f4
 Revises: 
-Create Date: 2024-10-09 22:32:30.330308
+Create Date: 2024-10-30 20:32:52.469099
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'bcdfe35a4c42'
+revision = '62a4abc131f4'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -21,7 +21,7 @@ def upgrade():
     op.create_table('users',
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('username', sa.String(), nullable=False),
-    sa.Column('_password_hash', sa.String(), nullable=False),
+    sa.Column('_password_hash', sa.String(), nullable=True),
     sa.Column('image_url', sa.String(), nullable=True),
     sa.Column('bio', sa.String(), nullable=True),
     sa.PrimaryKeyConstraint('id'),
@@ -29,10 +29,10 @@ def upgrade():
     )
     op.create_table('recipes',
     sa.Column('id', sa.Integer(), nullable=False),
+    sa.Column('user_id', sa.Integer(), nullable=True),
     sa.Column('title', sa.String(), nullable=False),
     sa.Column('instructions', sa.String(), nullable=False),
     sa.Column('minutes_to_complete', sa.Integer(), nullable=True),
-    sa.Column('user_id', sa.Integer(), nullable=False),
     sa.ForeignKeyConstraint(['user_id'], ['users.id'], name=op.f('fk_recipes_user_id_users')),
     sa.PrimaryKeyConstraint('id')
     )
